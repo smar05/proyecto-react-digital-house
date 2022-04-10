9126dc874834771fb86a5767ad662438a6850fa8
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-04-09 22:33:33 -0500

    Se crea el componente Producto

--- src/components/Web.js
+++ src/components/Web.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Encabezado from "./Encabezado";
 import Banner from "./Banner";
+import Producto from "./Producto";
 
 function Web() {
   return (
     <div className="container">
       <Encabezado />
       <Banner />
+      <Producto />
     </div>
   );
 }
