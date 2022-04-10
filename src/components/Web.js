9a716ec93c2ca51e9b559982e75e50a0a0e9fa1f
Ricardo Mantilla <mantillasanchezr@gmail.com>
2022-04-09 22:25:39 -0500

    Se crea el componente Banner

--- src/components/Web.js
+++ src/components/Web.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Encabezado from "./Encabezado";
+import Banner from "./Banner";
+
 function Web() {
   return (
     <div className="container">
       <Encabezado />
+      <Banner />
     </div>
   );
 }
